098222826de5215ec9086ad04472f033b6e6477b
Agung Adi Prasetio <prasetioxagung@gmail.com>
2020-11-14 18:07:43 +0700

    instal cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const PORT = 3000
 const app = express()
 const router = require('./routes')
 const errorHandler = require('./middlewares/error-handler')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(router)
